4fa86e379fb08d25ee1844d61a2532d8d60fd650
giovannyCaluna <gio-vanny1@hotmail.com>
2025-11-30 09:52:35 -0500

    Update deployment script to use --remove-orphans flag for better container management

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,7 +24,7 @@ docker pull giovacaluna96/tata:$IMAGE_TAG
 # Update the running service
 # We use docker-compose up -d to recreate containers with the new image
 # Note: This assumes docker-compose.yml is present in the same directory
-docker compose up -d --no-deps --build app
+docker compose -f docker-compose.yml up -d --remove-orphans
 
 # Prune old images to save space
 docker image prune -f
