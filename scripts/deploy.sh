7f5178cffb2e654438ac52ad6ab5d08792bc7c61
giovannyCaluna <gio-vanny1@hotmail.com>
2025-11-30 12:00:30 -0500

    Refactor deploy script: clean up comments and improve clarity

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,15 +15,14 @@ echo "Deploying version: $IMAGE_TAG"
 # Export variables for docker-compose
 export IMAGE_TAG=$IMAGE_TAG
 
-# Login to Docker Hub (optional if public, but good practice)
+# Login to Docker Hub 
 # echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 
 # Pull the new image
 docker pull giovacaluna96/tata:$IMAGE_TAG
 
 # Update the running service
-# We use docker-compose up -d to recreate containers with the new image
-# Note: This assumes docker-compose.yml is present in the same directory
+
 docker compose -f docker-compose.yml up -d --remove-orphans
 
 # Prune old images to save space
